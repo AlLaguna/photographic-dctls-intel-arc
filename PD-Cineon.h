e13155c79020f52e74799a00e63278954b12a8b1
Alvaro L <79445367+AlLaguna@users.noreply.github.com>
2026-09-20 11:44:50 +0200

    Enforce single precision to prevent double/float type conflicts on PD-Cineon-Invert.dctl to fix visual glitches on Intel Arc GPUs

--- PD-Cineon.h
+++ PD-Cineon.h
@@ -13,11 +13,11 @@ struct CineonColor {
 };
 
 __CONSTANT__ struct CineonColor cineon_colors[] = {
-    { -8, 0.01f, 0.01f, 0.01f }, { -7, 0.05f, 0.05f, 0.05f }, { -6, 0.10f, 0.10f, 0.10f }, { -5, 0.40f, 0.25f, 0.60f },
-    { -4, 0.20f, 0.45f, 0.70f }, { -3, 0.40f, 0.60f, 0.95f }, { -2, 0.40f, 0.60f, 0.25f }, { -1, 0.60f, 0.90f, 0.55f },
-    { 0, 0.50f, 0.50f, 0.50f },  { 1, 1.00f, 0.95f, 0.25f },  { 2, 0.90f, 0.50f, 0.25f },  { 3, 0.90f, 0.60f, 0.25f },
-    { 4, 0.90f, 0.30f, 0.20f },  { 5, 0.90f, 0.35f, 0.30f },  { 6, 0.90f, 0.90f, 0.90f },  { 7, 0.95f, 0.95f, 0.95f },
-    { 8, 0.99f, 0.99f, 0.99f }
+    { -8.0f, 0.01f, 0.01f, 0.01f }, { -7.0f, 0.05f, 0.05f, 0.05f }, { -6.0f, 0.10f, 0.10f, 0.10f }, { -5.0f, 0.40f, 0.25f, 0.60f }, 
+    { -4.0f, 0.20f, 0.45f, 0.70f }, { -3.0f, 0.40f, 0.60f, 0.95f }, { -2.0f, 0.40f, 0.60f, 0.25f }, { -1.0f, 0.60f, 0.90f, 0.55f },
+    { 0.0f, 0.50f, 0.50f, 0.50f },  { 1.0f, 1.00f, 0.95f, 0.25f },  { 2.0f, 0.90f, 0.50f, 0.25f },  { 3.0f, 0.90f, 0.60f, 0.25f }, 
+    { 4.0f, 0.90f, 0.30f, 0.20f },  { 5.0f, 0.90f, 0.35f, 0.30f },  { 6.0f, 0.90f, 0.90f, 0.90f },  { 7.0f, 0.95f, 0.95f, 0.95f },
+    { 8.0f, 0.99f, 0.99f, 0.99f }
 };
 #define cineon_stops 17
 
@@ -26,19 +26,19 @@ struct CineonCurve {
     float density;
     float gamma;
     float bitdepth;
-    int offset;
-    int white;
+    float offset; 
+    float white;  
 };
 
 __DEVICE__ struct CineonCurve
 cineon_curve()
 {
     struct CineonCurve cv;
-    cv.density = 2.046;
-    cv.gamma = 0.6;
-    cv.bitdepth = 1023;
-    cv.offset = 95;
-    cv.white = 685;
+    cv.density = 2.046f;
+    cv.gamma = 0.6f;
+    cv.bitdepth = 1023.0f; 
+    cv.offset = 95.0f;     
+    cv.white = 685.0f;     
     return cv;
 }
 
@@ -52,7 +52,8 @@ __DEVICE__ float3
 CineonCurve_invert_cineon(struct CineonCurve cv, float3 rgb, float3 scale, float3 dmin)
 {
     float3 invert = make_float3(cv.offset, cv.offset, cv.offset);
-    invert += scale * log10_f3(div_3f(dmin, rgb)) / CineonCurve_steps(cv);
+    
+    invert += scale * log10_f3(div_3f(dmin, rgb)) / CineonCurve_steps(cv); 
     invert /= cv.bitdepth;
     return invert;
 }
@@ -64,7 +65,8 @@ CineonCurve_reverse_cineon(struct CineonCurve cv, float3 rgb, float3 scale, floa
     reverse *= cv.bitdepth;
     reverse -= make_float3(cv.offset, cv.offset, cv.offset);
     float3 scaled = reverse * CineonCurve_steps(cv) / scale;
-    rgb = dmin / pow10_f3(scaled);
+    
+    rgb = dmin / max_f3(pow10_f3(scaled), 1e-7f); 
     return rgb;
 }
 
@@ -72,11 +74,13 @@ __DEVICE__ float3
 CineonCurve_lin_cineon(struct CineonCurve cv, float3 rgb)
 {
     float scale = CineonCurve_steps(cv) / cv.gamma;
-    float gain = 1.0 - pow10_f((cv.offset - cv.white) * scale);
+    float gain = 1.0f - pow10_f((cv.offset - cv.white) * scale);
     float offset = cv.white / cv.bitdepth;
-    float r = offset + log_f(max_f((rgb.x - 1.0f) * gain + 1.0, 1e-7)) / (cv.bitdepth * log_f(10) * scale);
-    float g = offset + log_f(max_f((rgb.y - 1.0f) * gain + 1.0, 1e-7)) / (cv.bitdepth * log_f(10) * scale);
-    float b = offset + log_f(max_f((rgb.z - 1.0f) * gain + 1.0, 1e-7)) / (cv.bitdepth * log_f(10) * scale);
+    
+    // log_f está protegido en PD-Common.h
+    float r = offset + log_f(max_f((rgb.x - 1.0f) * gain + 1.0f, 1e-7f)) / (cv.bitdepth * log_f(10.0f) * scale); 
+    float g = offset + log_f(max_f((rgb.y - 1.0f) * gain + 1.0f, 1e-7f)) / (cv.bitdepth * log_f(10.0f) * scale); 
+    float b = offset + log_f(max_f((rgb.z - 1.0f) * gain + 1.0f, 1e-7f)) / (cv.bitdepth * log_f(10.0f) * scale); 
     rgb = make_float3(r, g, b);
     return rgb;
 }
@@ -87,7 +91,7 @@ CineonCurve_cineon_lin(struct CineonCurve cv, float3 rgb)
     float scale = CineonCurve_steps(cv) / cv.gamma;
     rgb = min_f3(rgb, 1.0f) * cv.bitdepth;
     float black = pow_f(10.0f, cv.offset * scale);
-    float diff = pow_f(10.0f, cv.white * scale) - black;
+    float diff = max_f(pow_f(10.0f, cv.white * scale) - black, 1e-7f); 
     float r = pow_f(10.0f, rgb.x * scale);
     float g = pow_f(10.0f, rgb.y * scale);
     float b = pow_f(10.0f, rgb.z * scale);
